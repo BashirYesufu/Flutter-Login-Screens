58b272e4f3cd2ec900fc227a50a901c38a987727
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-18 00:23:39 +0100

    Laid out components on the voluntary login screen

--- kobo_login_screens/lib/screens/voluntary_login.dart
+++ kobo_login_screens/lib/screens/voluntary_login.dart
@@ -12,6 +12,128 @@ class VoluntaryLogin extends StatefulWidget {
 class _VoluntaryLoginState extends State<VoluntaryLogin> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        padding: EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+            Spacer(),
+
+            Image.asset('images/voluntary_login.png'),
+
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 40.0, horizontal: 20.0),
+              color: Colors.grey,
+              height: 1.0,
+            ),
+
+            Text(
+              'Log in or create an account to keep your \nsubscription in sync',
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+              textAlign: TextAlign.center,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15.0),
+              child: Material(
+                color: Color(0xFF1A1A1A),
+                borderRadius: BorderRadius.circular(5.0),
+                child: MaterialButton(
+                  onPressed: () {},
+                  minWidth: double.infinity,
+                  height: 50.0,
+                  child: Text(
+                    'Login with Facebook',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15.0),
+                    child: Material(
+                      color: Color(0xFF1A1A1A),
+                      borderRadius: BorderRadius.circular(5.0),
+                      child: MaterialButton(
+                        onPressed: () {},
+
+                        height: 50.0,
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(width: 20.0,),
+
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15.0),
+                    child: Material(
+                      color: Color(0xFF1A1A1A),
+                      borderRadius: BorderRadius.circular(5.0),
+                      child: MaterialButton(
+                        onPressed: () {},
+
+                        height: 50.0,
+                        child: Text(
+                          'Log In',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 150.0,),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15.0),
+              child: Material(
+                color: Color(0xFF1A1A1A),
+                borderRadius: BorderRadius.circular(5.0),
+                child: MaterialButton(
+                  onPressed: () {},
+                  minWidth: double.infinity,
+                  height: 50.0,
+                  child: Text(
+                    'Later',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
