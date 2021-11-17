514c115f7c511d7a02ce96afbc588f922b1e58db
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-18 00:38:04 +0100

    Added border color to the Facebook login button on the voluntary login

--- kobo_login_screens/lib/screens/voluntary_login.dart
+++ kobo_login_screens/lib/screens/voluntary_login.dart
@@ -43,9 +43,12 @@ class _VoluntaryLoginState extends State<VoluntaryLogin> {
               padding: const EdgeInsets.symmetric(vertical: 15.0),
               child: Material(
                 color: Color(0xFF1A1A1A),
-                borderRadius: BorderRadius.circular(5.0),
                 child: MaterialButton(
                   onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
                   minWidth: double.infinity,
                   height: 50.0,
                   child: Text(
@@ -116,7 +119,7 @@ class _VoluntaryLoginState extends State<VoluntaryLogin> {
               padding: const EdgeInsets.symmetric(vertical: 15.0),
               child: Material(
                 color: Color(0xFF1A1A1A),
-                borderRadius: BorderRadius.circular(5.0),
+                borderRadius: BorderRadius.circular(10.0),
                 child: MaterialButton(
                   onPressed: () {},
                   minWidth: double.infinity,
