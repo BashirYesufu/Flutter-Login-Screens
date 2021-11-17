1df906ce406494ff5fc4bc4837e7d747bd597785
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-18 00:56:13 +0100

    Added vault image and welcome text to goliath screen

--- kobo_login_screens/lib/screens/goliath_bank.dart
+++ kobo_login_screens/lib/screens/goliath_bank.dart
@@ -13,7 +13,38 @@ class _GoliathBankState extends State<GoliathBank> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset('images/goliath_bank.png'),
 
+            SizedBox(height: 20.0,),
+
+            Text(
+              'Welcome to',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 10.0,),
+
+            Text(
+              'Goliath National Bank',
+              style: TextStyle(
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+
+          ],
+        ),
+      ),
     );
   }
 }
