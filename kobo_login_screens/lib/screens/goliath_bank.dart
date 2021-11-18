4e78f7ef30dd95e2d9915d78cf64bc56ef1d8591
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-18 01:10:15 +0100

    Added placeholder text fields and other components to the goliath screen

--- kobo_login_screens/lib/screens/goliath_bank.dart
+++ kobo_login_screens/lib/screens/goliath_bank.dart
@@ -14,11 +14,14 @@ class _GoliathBankState extends State<GoliathBank> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.all(20.0),
+        padding: EdgeInsets.all(40.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+
+            Spacer(),
+
             Image.asset('images/goliath_bank.png'),
 
             SizedBox(height: 20.0,),
@@ -41,7 +44,63 @@ class _GoliathBankState extends State<GoliathBank> {
               ),
             ),
 
+            TextField(),
 
+            TextField(),
+
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                'Forgot Password?',
+                style: TextStyle(
+                  color: Color(0xFF004AFF),
+                  fontSize: 12.0,
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15.0),
+              child: Material(
+                color: Color(0xFF5179D3),
+                borderRadius: BorderRadius.circular(5.0),
+                child: MaterialButton(
+                  onPressed: () {},
+                  minWidth: double.infinity,
+                  height: 50.0,
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 200.0,),
+
+            Center(
+              child: Column(
+                children: [
+                  Text('First time user?'),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Forgot Password?',
+                      style: TextStyle(
+                        color: Color(0xFF004AFF),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 20.0,),
           ],
         ),
       ),
